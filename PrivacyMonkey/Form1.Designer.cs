232c7128948418d130acbc8e9638006cb9fe1738
Nav Aulakh <nav@navaulakh.com>
2016-03-01 21:46:41 -0800

    Added code to repository

---
+++ PrivacyMonkey/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace PrivacyMonkey
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.txtSelectedFile = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBank = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblWait = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lstOutputFiles = new System.Windows.Forms.ListBox();
+            this.chkConfirm = new System.Windows.Forms.CheckBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelect.Location = new System.Drawing.Point(128, 76);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(88, 23);
+            this.btnSelect.TabIndex = 1;
+            this.btnSelect.Text = "Select file";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // txtSelectedFile
+            // 
+            this.txtSelectedFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSelectedFile.Location = new System.Drawing.Point(231, 76);
+            this.txtSelectedFile.Name = "txtSelectedFile";
+            this.txtSelectedFile.ReadOnly = true;
+            this.txtSelectedFile.Size = new System.Drawing.Size(338, 22);
+            this.txtSelectedFile.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Bank";
+            // 
+            // txtBank
+            // 
+            this.txtBank.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBank.Location = new System.Drawing.Point(128, 36);
+            this.txtBank.Name = "txtBank";
+            this.txtBank.Size = new System.Drawing.Size(441, 22);
+            this.txtBank.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(15, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Statement";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblWait);
+            this.groupBox1.Controls.Add(this.btnConvert);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtSelectedFile);
+            this.groupBox1.Controls.Add(this.btnSelect);
+            this.groupBox1.Controls.Add(this.txtBank);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(590, 166);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            // 
+            // lblWait
+            // 
+            this.lblWait.AutoSize = true;
+            this.lblWait.Location = new System.Drawing.Point(266, 127);
+            this.lblWait.Name = "lblWait";
+            this.lblWait.Size = new System.Drawing.Size(0, 13);
+            this.lblWait.TabIndex = 8;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Enabled = false;
+            this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvert.Location = new System.Drawing.Point(128, 122);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(131, 23);
+            this.btnConvert.TabIndex = 7;
+            this.btnConvert.Text = "Convert to PNG";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.lstOutputFiles);
+            this.groupBox2.Location = new System.Drawing.Point(12, 184);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(590, 157);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Files to be sent";
+            // 
+            // lstOutputFiles
+            // 
+            this.lstOutputFiles.FormattingEnabled = true;
+            this.lstOutputFiles.Location = new System.Drawing.Point(18, 32);
+            this.lstOutputFiles.Name = "lstOutputFiles";
+            this.lstOutputFiles.Size = new System.Drawing.Size(551, 108);
+            this.lstOutputFiles.TabIndex = 0;
+            // 
+            // chkConfirm
+            // 
+            this.chkConfirm.AutoSize = true;
+            this.chkConfirm.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkConfirm.Location = new System.Drawing.Point(87, 360);
+            this.chkConfirm.Name = "chkConfirm";
+            this.chkConfirm.Size = new System.Drawing.Size(441, 20);
+            this.chkConfirm.TabIndex = 1;
+            this.chkConfirm.Text = "I have removed personally identifying information from the output file(s)";
+            this.chkConfirm.UseVisualStyleBackColor = true;
+            this.chkConfirm.CheckedChanged += new System.EventHandler(this.chkConfirm_CheckedChanged);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Enabled = false;
+            this.btnSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSend.Image = ((System.Drawing.Image)(resources.GetObject("btnSend.Image")));
+            this.btnSend.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSend.Location = new System.Drawing.Point(115, 401);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(380, 45);
+            this.btnSend.TabIndex = 9;
+            this.btnSend.Text = "       Send email to Nav securely and anonymously";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 477);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.chkConfirm);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Privacy Monkey for Perfin";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.TextBox txtSelectedFile;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBank;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox chkConfirm;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ListBox lstOutputFiles;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblWait;
+    }
+}
+
---
+++ PrivacyMonkey/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Mail;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ImageMagick;
+
+namespace PrivacyMonkey
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog ofd = new OpenFileDialog();
+        String bank, selectedFile, selectedSafeFile;
+        List<String> outputfiles = new List<String>();
+        static String fromEmail = "onewebdev@gmail.com";
+        static String toEmail = "nav@navaulakh.com";
+
+        SmtpClient smtp = new SmtpClient
+        {
+            Host = "smtp.gmail.com",
+            Port = 587,
+            EnableSsl = true,
+            DeliveryMethod = SmtpDeliveryMethod.Network,
+            UseDefaultCredentials = false,
+            Credentials = new NetworkCredential(fromEmail, "xxx")
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            txtSelectedFile.Text = "";
+        }
+
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            ofd.Filter = "PDF|*.pdf";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                selectedFile = ofd.FileName;
+                selectedSafeFile = ofd.SafeFileName;
+                txtSelectedFile.Text = selectedFile;
+                btnConvert.Enabled = true;
+            }
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            bank = txtBank.Text;
+            if (bank == null || selectedFile == null)
+            {
+                MessageBox.Show(
+                    "Please enter bank name and statement file",
+                    "Missing Information",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+                return;
+            }
+
+            MagickReadSettings settings = new MagickReadSettings();
+
+            settings.Density = new PointD(300, 300);
+            String outputfile;
+            System.Diagnostics.ProcessStartInfo procInfo = new System.Diagnostics.ProcessStartInfo();
+            procInfo.FileName = ("mspaint.exe");
+
+            using (MagickImageCollection images = new MagickImageCollection())
+            {
+                images.Read(selectedFile, settings);
+                int page = 1;
+                foreach (MagickImage image in images)
+                {
+                    outputfile = String.Format(@"{0}\output-page-{1}.png", Path.GetDirectoryName(selectedFile), page);
+                    image.Write(outputfile);
+                    outputfiles.Add(outputfile);
+                    displayOutputFiles();
+
+                    procInfo.Arguments = outputfile;
+                    System.Diagnostics.Process.Start(procInfo);
+
+                    page++;
+                }
+            }
+
+
+
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            using (var message = new MailMessage(fromEmail, toEmail)
+            {
+                Subject = "A kind person has sent you a statement",
+                Body = "Financial Institution: " + bank
+            })
+            {
+                btnSend.Enabled = false;
+                for (int i = 0; i < outputfiles.Count; i++)
+                {
+                    message.Attachments.Add(new Attachment(outputfiles[i]));
+                }
+                smtp.Send(message);
+                MessageBox.Show("Email sent successfully. Thanks for your help!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                btnSend.Enabled = true;
+            }
+        }
+
+        private void chkConfirm_CheckedChanged(object sender, EventArgs e)
+        {
+            btnSend.Enabled = chkConfirm.Checked && outputfiles.Count > 0;
+        }
+
+        private void displayOutputFiles()
+        {
+            lstOutputFiles.Items.Clear();
+            for (int i = 0; i < outputfiles.Count; i++)
+            {
+                lstOutputFiles.Items.Add(outputfiles[i]);
+            }
+        }
+    }
+}
